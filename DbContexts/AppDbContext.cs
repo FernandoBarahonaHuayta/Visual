3c65696907913a1969700f06e8819ff3026d4a7e
fernandobarahonahuayta <fernandobarahonahuayta@gmail.com>
2023-05-03 19:22:45 -0500

    first commit

---
+++ DbContexts/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Ejercicio02.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ejercicio02.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("ProductBD");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+}
